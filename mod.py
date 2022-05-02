fcf543f3804da50383f5febb33ec52c7e2f86a73
kamrynbrown <104734461+kamrynbrown@users.noreply.github.com>
2022-05-02 11:52:29 -0400

    Update mod.py

--- mod.py
+++ mod.py
@@ -2,9 +2,7 @@
 import sys
 
 for line in sys.stdin:
-  encryption_option = input("Would you like to Encode or Decode? ") 
   should_encode = "encode" in encryption_option.lower()
-  should_decode = "decode" in encryption_option.lower()
   cypher_number = int(input("What is your cypher number? "))
   message = input("What is your message? ")
   encoded_message = ""
@@ -22,24 +20,6 @@ for line in sys.stdin:
               if next_char_index >122:
                 overflow = next_char_index - 122
                 next_char_index = 96 + overflow
-              encoded_message = encoded_message + chr(next_char_index)
-          else:
-              encoded_message += char
-      print(encoded_message)
-
-  elif should_decode:
-      for char in message:
-          if ord(char) >=65 and ord(char)<=90:
-              next_char_index = ord(char) - cypher_number
-              if next_char_index <65:
-                underflow = 65 - next_char_index
-                next_char_index = 91 - underflow
-              encoded_message = encoded_message + chr(next_char_index)
-          elif ord(char) >=97 and ord(char)<=122:
-              next_char_index = ord(char) - cypher_number
-              if next_char_index <97:
-                underflow = 97 - next_char_index
-                next_char_index = 123 - underflow
               encoded_message = encoded_message + chr(next_char_index)
           else:
               encoded_message += char
